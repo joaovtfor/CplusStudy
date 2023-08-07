4d6a531d9d5e149174cd2a9e6eaa9c84f5cf04f1
joaovtfor <joaovtfor@hotmail.com>
2023-08-07 20:34:44 -0300

    `Add` - Hello World

---
+++ FirstClass/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Comment in C++
+
+/*
+    Multi
+        Lines
+            Comment
+*/
+
+using namespace std;
+
+int main()
+{
+cout << "Hello, World!" << endl; // endl forces a line-break
+ return 0;
+}
