381ddefdc784276e7777b67b8b67d127990eebe6
joaovtfor <joaovtfor@hotmail.com>
2023-08-07 21:42:16 -0300

    `Add` - Exercise 2

---
+++ FirstClass/Ex2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    string nome;
+    cout << "Digite o seu nome: ";
+    getline(cin, nome);
+    cout << "Menu nome é " << nome << endl;
+    return 0;
+}
